ca26e7bfcecc6907eb7a6042c4033d95239d65dc
d4vd59 <david.muhic@edu.bib.de>
2025-03-24 13:53:32 +0100

    test2

--- stundenplan/stundenplan.py
+++ stundenplan/stundenplan.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Hello GUI")
+root.geometry("400x300")  
+
+label = tk.Label(root, text="Hello, World!")
+label.pack()
+
+root.mainloop()
