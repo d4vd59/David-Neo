b47fade4c779d6af894779bde0936fd216582337
d4vd59 <david.muhic@edu.bib.de>
2025-03-25 09:50:10 +0100

    1

--- stundenplan/stundenplan.py
+++ stundenplan/stundenplan.py
@@ -1,14 +1,23 @@
 import tkinter as tk
+from ics import Calendar
+import requests
 
 root = tk.Tk()
-root.title("Hello GUI")
-root.geometry("400x300")  
+root.title("Mein Stundenplan")
+root.geometry("600x400")
 
-label = tk.Label(root, text="Hello, World!")
-label.pack()
+termine_text = tk.Text(root, wrap=tk.WORD)
+termine_text.pack(expand=True, fill="both")
 
-label2 = tk.Label(root, text="Moin")
-label2.pack(side="bottom")
+url = "https://intranet.bib.de/ical/5780139642c4ec77bd1f67ec885f2e92"
 
+response = requests.get(url)
+response.raise_for_status()  
+
+kalender = Calendar(response.text)
+events_sorted = sorted(kalender.events, key=lambda e: e.begin)
+
+for event in events_sorted:
+    termine_text.insert(tk.END, f"{event.begin.format('DD.MM.YYYY HH:mm')} - {event.name}\n")
 
 root.mainloop()
